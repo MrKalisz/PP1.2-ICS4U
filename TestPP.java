5c735fae2c64af46542c42b5f291a4e914d40e5d
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-02-04 00:18:30 -0500

    Create TestPP.java

---
+++ TestPP.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestPP {
+  
+  @Test
+  void Testfunc() {
+    assertEquals(1, PP.func(1));
+  }
+}
+
+
